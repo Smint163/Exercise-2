b0f62d56e3df4bdc6b1b7f2769aa406ffd979642
Smint <smint-russia@mail.ru>
2022-12-10 23:52:33 +0300

    For hub

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1");
+string number0 = Console.ReadLine();
+Console.WriteLine("Введите число 2");
+string number1 = Console.ReadLine();
+int num0 = Convert.ToInt32(number0);
+int num1 = Convert.ToInt32(number1);
+
+if (num0 > num1)
+{
+    Console.WriteLine($"max = {num0}, min = {num1}");
+}
+else 
+Console.WriteLine($"max = {num1}, min = {num0}");
